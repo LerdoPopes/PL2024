e2656eff6e98bf50b2aa356d88710cbf016604b8
LerdoPopes <pedroafonsoml@gmail.com>
2024-02-27 15:05:05 +0000

    TPC3 feito

---
+++ TP3/README.md
@@ -0,0 +1,13 @@
+PL2024
+
+Título: TPC3 De PL
+
+Autor: Pedro Afonso Moreira Lopes, A100759
+
+Explicação da resolução do exercício:
+
+Para este exercício, temos que criar um programa que some todas as sequências de dígitos que encontre num texto. Para este fim, foram utilizadas expressões regulares (Regex) para a leitura e adição dos elementos textuais necessários para a resolução do exercício numa lista, como se pode ver na função **somador**
+
+Estando todos os elementos necessários nessa lista, depois é feita uma travessia por essa lista, somando sempre que possível as sequências de números ao contador total e devolvendo o valor da soma também, tal como o exercício pediu.
+
+Para a execução do código, este foi executado como normal e funciona através da leitura do ficheiro "input.txt", que é onde se situam os inputs para o programa.
---
+++ TP3/TPC3.py
@@ -0,0 +1,59 @@
+import re 
+import sys
+
+def somador(input):
+    
+    contador = 0
+    #numero_string = ""
+    #numero_inteiro = 0
+    estado = False
+    
+    lista_final = []
+    
+    #Versão que soma o contador número a número 
+    with open(input,"r") as ficheiro:
+        
+        # Adicionar todos os On/Off/=/números numa lista
+        for line in ficheiro:
+            capture = re.findall(r'(on|off|=|\d)', line, re.IGNORECASE)
+            lista_final.extend(capture)
+        print(lista_final)
+        for elemento in lista_final:
+            if elemento == "=":
+                print(contador) 
+            elif elemento.isnumeric() and estado == True:
+                contador += int(elemento)
+            elif elemento.upper() == "OFF":
+                estado = False
+            elif elemento.upper() == "ON":
+                estado = True        
+
+    #with open(input,"r") as ficheiro:
+    #    for line in ficheiro:
+    #        capture = re.findall(r'(on|off|=|\d)', line, re.IGNORECASE)
+    #        lista_final.extend(capture)
+    #    print(lista_final)
+    #    for elemento in lista_final:
+    #        if elemento == "=":
+    #            print(contador) 
+    #        elif elemento.isnumeric() and estado == True:
+    #           numero_string += elemento
+    #        elif elemento.upper() == "OFF" and estado == True:
+    #            numero_inteiro = int(numero_string)
+    #            numero_string = ""
+    #            contador += numero_inteiro
+    #            estado = False
+    #        elif elemento.upper() == "ON" and estado == True:
+    #            numero_inteiro = int(numero_string)
+    #            numero_string = ""
+    #            contador += numero_inteiro
+    #            estado = True
+    #        elif elemento.upper() == "ON" and estado == False:
+    #            estado == True
+    #        elif elemento.upper() == "OFF" and estado == False:
+    #            estado == False
+
+                
+if __name__ == "__main__":
+    
+    somador("input.txt")
