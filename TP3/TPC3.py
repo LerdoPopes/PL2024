7d963a5ec671b64239d125478d445fcd4d7e9742
LerdoPopes <pedroafonsoml@gmail.com>
2024-02-27 17:49:15 +0000

    TPC Corrigido

--- TP3/TPC3.py
+++ TP3/TPC3.py
@@ -15,8 +15,9 @@ def somador(input):
         
         # Adicionar todos os On/Off/=/números numa lista
         for line in ficheiro:
-            capture = re.findall(r'(on|off|=|\d)', line, re.IGNORECASE)
+            capture = re.findall(r'(on|off|=|\d+)', line, re.IGNORECASE)
             lista_final.extend(capture)
+        print(lista_final)
         for elemento in lista_final:
             if elemento == "=":
                 print("Total atual : " + str(contador)) 
@@ -26,32 +27,6 @@ def somador(input):
                 estado = False
             elif elemento.upper() == "ON":
                 estado = True        
-
-    #with open(input,"r") as ficheiro:
-    #    for line in ficheiro:
-    #        capture = re.findall(r'(on|off|=|\d)', line, re.IGNORECASE)
-    #        lista_final.extend(capture)
-    #    print(lista_final)
-    #    for elemento in lista_final:
-    #        if elemento == "=":
-    #            print(contador) 
-    #        elif elemento.isnumeric() and estado == True:
-    #           numero_string += elemento
-    #        elif elemento.upper() == "OFF" and estado == True:
-    #            numero_inteiro = int(numero_string)
-    #            numero_string = ""
-    #            contador += numero_inteiro
-    #            estado = False
-    #        elif elemento.upper() == "ON" and estado == True:
-    #            numero_inteiro = int(numero_string)
-    #            numero_string = ""
-    #            contador += numero_inteiro
-    #            estado = True
-    #        elif elemento.upper() == "ON" and estado == False:
-    #            estado == True
-    #        elif elemento.upper() == "OFF" and estado == False:
-    #            estado == False
-
                 
 if __name__ == "__main__":
     
